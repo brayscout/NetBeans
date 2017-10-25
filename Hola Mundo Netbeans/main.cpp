5bd0d8bb65df073bb1d56bdf01eea03c0366c104
brayscout <32906559+brayscout@users.noreply.github.com>
2017-10-25 01:20:39 -0500

    Hola Mundo NetBeans

---
+++ Hola Mundo Netbeans/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: BRYAN TUALLE
+ *
+ * Created on 25 de octubre de 2017, 0:18
+ */
+
+//#include <cstdlib>
+//#include <stddef.h>
+#include <stdlib.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf("Hola Mundo de Netbeans");
+
+    return 0;
+}
+
